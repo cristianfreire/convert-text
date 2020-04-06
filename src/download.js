b79f5b575b0307beb9927850fd105b2ff57cb89e
Cristian Freire <cristianpassos51@gmail.com>
2020-04-06 10:23:57 -0400

    Feito

--- src/download.js
+++ src/download.js
@@ -17,6 +17,64 @@ function download() {
     downloadLink.click();
 }
 
+function downResult() {
+    var textToSave = document.getElementById("result").value;
+    var textToSaveAsBlob = new Blob([textToSave], {
+        type: "text/plain"
+    });
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var fileNameToSaveAs = "convert-text"
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+
+    downloadLink.click();
+}
+
+function downBin() {
+    var textToSave = document.getElementById("binario").value;
+    var textToSaveAsBlob = new Blob([textToSave], {
+        type: "text/plain"
+    });
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var fileNameToSaveAs = "convert-text"
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+
+    downloadLink.click();
+}
+
+
+function downMorse() {
+    var textToSave = document.getElementById("morse").value;
+    var textToSaveAsBlob = new Blob([textToSave], {
+        type: "text/plain"
+    });
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var fileNameToSaveAs = "convert-text"
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+
+    downloadLink.click();
+}
+
 function destroyClickedElement(event) {
     document.body.removeChild(event.target);
 }
